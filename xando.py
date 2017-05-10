48ed427b2864fae2cf71b5b8acb54df39488e885
James Maher <maher.j@live.com>
2017-05-10 13:44:56 +0100

    Moved the formatting and colours to global scope.
    Separated the square updating to a separate function.

--- xando.py
+++ xando.py
@@ -1,24 +1,29 @@
 import pygame, sys
 from pygame.locals import *
 
-def main():
-    pygame.init()
-    # Square Details
-    thesquares = [(x,0) for x in range(9)]
-    SQUARE_LENGTH = 50
-    SQUARE_MARGIN = 10
-    # Colours
-    WHITE = (255,255,255)
-    BLUE = (80, 80, 183)
-    YELLOW = (206, 244, 66)
+# Square Formatting
+SQUARE_LENGTH = 50
+SQUARE_MARGIN = 10
+# Colours
+WHITE = (255,255,255)
+BLUE = (80, 80, 183)
+YELLOW = (206, 244, 66)
 
-    DISPLAYSURF = pygame.display.set_mode((400,400))
-    for square in thesquares:
-        pygame.draw.rect(DISPLAYSURF,
+def update_squares(display, the_squares):
+    for square in the_squares:
+        pygame.draw.rect(display,
                          WHITE,
                          ((square[0]%3)*(SQUARE_LENGTH+SQUARE_MARGIN)+SQUARE_MARGIN,
                           (square[0]//3)*(SQUARE_LENGTH+SQUARE_MARGIN) +SQUARE_MARGIN,
                           SQUARE_LENGTH, SQUARE_LENGTH))
+
+def main():
+    pygame.init()
+    # Square Details
+    the_squares = [(x,0) for x in range(9)]
+
+    DISPLAYSURF = pygame.display.set_mode((400,400))
+    update_squares(DISPLAYSURF, the_squares)
 
     while True:
         for event in pygame.event.get():
