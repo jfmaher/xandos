2c9edcc804390491aec3fcde1eb23aa12fab1954
James Maher <maher.j@live.com>
2017-05-10 14:16:22 +0100

    Added a mousedown section in event loop.
    Change the variable the_sqaures to contain Rect objects.

--- xando.py
+++ xando.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from pygame.locals import *
+from operator import __sub__ as sub
 
 # Square Formatting
 SQUARE_LENGTH = 50
@@ -13,20 +14,27 @@ def update_squares(display, the_squares):
     for square in the_squares:
         pygame.draw.rect(display,
                          WHITE,
-                         ((square[0]%3)*(SQUARE_LENGTH+SQUARE_MARGIN)+SQUARE_MARGIN,
-                          (square[0]//3)*(SQUARE_LENGTH+SQUARE_MARGIN) +SQUARE_MARGIN,
-                          SQUARE_LENGTH, SQUARE_LENGTH))
+                         square)
 
 def main():
     pygame.init()
     # Square Details
-    the_squares = [(x,0) for x in range(9)]
+    the_squares = []
+    for i in range(9):
+        the_squares = the_squares + [Rect(((i%3)*(SQUARE_LENGTH+SQUARE_MARGIN)+SQUARE_MARGIN,
+                                     (i//3)*(SQUARE_LENGTH+SQUARE_MARGIN) +SQUARE_MARGIN,
+                                     SQUARE_LENGTH, SQUARE_LENGTH))]
 
     DISPLAYSURF = pygame.display.set_mode((400,400))
     update_squares(DISPLAYSURF, the_squares)
 
     while True:
         for event in pygame.event.get():
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    pass
+                else:
+                    pass
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
